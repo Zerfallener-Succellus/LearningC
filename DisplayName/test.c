59901335c8567bc0bed8e7fcfebe84c5b8419c8c
Gawain <pedrolucf@gmail.com>
2024-10-17 15:08:30 -0300

    first commit

---
+++ DisplayName/test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[20];
+    printf("Whats your name ? :\n");
+    scanf("%s", name);
+    printf("Hi, my name is: %s\n", name);
+    return 0;
+}
---
+++ OurFirstProgram/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>\
+
+int main() {
+    int myFavoriteNumber = 0;
+
+    printf("Enter your favorite number: \n");
+    scanf("%d", &myFavoriteNumber);
+    printf("The number you entered is: %d\n", myFavoriteNumber);
+
+    return 0;
+}
